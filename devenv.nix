33c8adca05c79d3d2409c92b7d526e9479fda6d4
Kyle Cierzan <kcierzan@gmail.com>
2025-11-03 19:45:51 -0500

    Fix builds on nixos

--- devenv.nix
+++ devenv.nix
@@ -23,91 +23,86 @@ let
   linuxTargetPrefix = linuxCrossCc.targetPrefix or "";
   linuxPkgs = inputs.nixpkgs.legacyPackages."x86_64-linux";
   linuxXorg = if linuxPkgs ? xorg then linuxPkgs.xorg else { };
-  linuxPkgConfigPaths =
-    lib.concatLists [
-      (lib.optionals (linuxPkgs ? libGL) [
-        "${linuxPkgs.libGL.dev}/lib/pkgconfig"
-      ])
-      (lib.optionals (linuxXorg ? libX11) [
-        "${linuxXorg.libX11.dev}/lib/pkgconfig"
-      ])
-      (lib.optionals (linuxXorg ? libxcb) [
-        "${linuxXorg.libxcb.dev}/lib/pkgconfig"
-      ])
-      (lib.optionals (linuxXorg ? xcbutil) [
-        "${linuxXorg.xcbutil.dev}/lib/pkgconfig"
-      ])
-      (lib.optionals (linuxXorg ? xcbutilimage) [
-        "${linuxXorg.xcbutilimage.dev}/lib/pkgconfig"
-      ])
-      (lib.optionals (linuxXorg ? xcbutilrenderutil) [
-        "${linuxXorg.xcbutilrenderutil.dev}/lib/pkgconfig"
-      ])
-      (lib.optionals (linuxXorg ? xcbutilwm) [
-        "${linuxXorg.xcbutilwm.dev}/lib/pkgconfig"
-      ])
-      (lib.optionals (linuxXorg ? xcbutilkeysyms) [
-        "${linuxXorg.xcbutilkeysyms.dev}/lib/pkgconfig"
-      ])
-      (lib.optionals (linuxXorg ? libXcursor) [
-        "${linuxXorg.libXcursor.dev}/lib/pkgconfig"
-      ])
-      (lib.optionals (linuxXorg ? libXi) [
-        "${linuxXorg.libXi.dev}/lib/pkgconfig"
-      ])
-      (lib.optionals (linuxXorg ? libXrandr) [
-        "${linuxXorg.libXrandr.dev}/lib/pkgconfig"
-      ])
-      (lib.optionals (linuxPkgs ? xorgproto) [
-        "${linuxPkgs.xorgproto}/share/pkgconfig"
-      ])
-    ];
-  linuxLibraryPaths =
-    lib.concatLists [
-      (lib.optionals (linuxPkgs ? libGL) [
-        "${linuxPkgs.libGL.out}/lib"
-      ])
-      (lib.optionals (linuxXorg ? libX11) [
-        "${linuxXorg.libX11.out}/lib"
-      ])
-      (lib.optionals (linuxXorg ? libxcb) [
-        "${linuxXorg.libxcb.out}/lib"
-      ])
-      (lib.optionals (linuxXorg ? xcbutil) [
-        "${linuxXorg.xcbutil.out}/lib"
-      ])
-      (lib.optionals (linuxXorg ? xcbutilimage) [
-        "${linuxXorg.xcbutilimage.out}/lib"
-      ])
-      (lib.optionals (linuxXorg ? xcbutilrenderutil) [
-        "${linuxXorg.xcbutilrenderutil.out}/lib"
-      ])
-      (lib.optionals (linuxXorg ? xcbutilwm) [
-        "${linuxXorg.xcbutilwm.out}/lib"
-      ])
-      (lib.optionals (linuxXorg ? xcbutilkeysyms) [
-        "${linuxXorg.xcbutilkeysyms.out}/lib"
-      ])
-      (lib.optionals (linuxXorg ? libXcursor) [
-        "${linuxXorg.libXcursor.out}/lib"
-      ])
-      (lib.optionals (linuxXorg ? libXi) [
-        "${linuxXorg.libXi.out}/lib"
-      ])
-      (lib.optionals (linuxXorg ? libXrandr) [
-        "${linuxXorg.libXrandr.out}/lib"
-      ])
-    ];
+  linuxPkgConfigPaths = lib.concatLists [
+    (lib.optionals (linuxPkgs ? libGL) [
+      "${linuxPkgs.libGL.dev}/lib/pkgconfig"
+    ])
+    (lib.optionals (linuxXorg ? libX11) [
+      "${linuxXorg.libX11.dev}/lib/pkgconfig"
+    ])
+    (lib.optionals (linuxXorg ? libxcb) [
+      "${linuxXorg.libxcb.dev}/lib/pkgconfig"
+    ])
+    (lib.optionals (linuxXorg ? xcbutil) [
+      "${linuxXorg.xcbutil.dev}/lib/pkgconfig"
+    ])
+    (lib.optionals (linuxXorg ? xcbutilimage) [
+      "${linuxXorg.xcbutilimage.dev}/lib/pkgconfig"
+    ])
+    (lib.optionals (linuxXorg ? xcbutilrenderutil) [
+      "${linuxXorg.xcbutilrenderutil.dev}/lib/pkgconfig"
+    ])
+    (lib.optionals (linuxXorg ? xcbutilwm) [
+      "${linuxXorg.xcbutilwm.dev}/lib/pkgconfig"
+    ])
+    (lib.optionals (linuxXorg ? xcbutilkeysyms) [
+      "${linuxXorg.xcbutilkeysyms.dev}/lib/pkgconfig"
+    ])
+    (lib.optionals (linuxXorg ? libXcursor) [
+      "${linuxXorg.libXcursor.dev}/lib/pkgconfig"
+    ])
+    (lib.optionals (linuxXorg ? libXi) [
+      "${linuxXorg.libXi.dev}/lib/pkgconfig"
+    ])
+    (lib.optionals (linuxXorg ? libXrandr) [
+      "${linuxXorg.libXrandr.dev}/lib/pkgconfig"
+    ])
+    (lib.optionals (linuxPkgs ? xorgproto) [
+      "${linuxPkgs.xorgproto}/share/pkgconfig"
+    ])
+  ];
+  linuxLibraryPaths = lib.concatLists [
+    (lib.optionals (linuxPkgs ? libGL) [
+      "${linuxPkgs.libGL.out}/lib"
+    ])
+    (lib.optionals (linuxXorg ? libX11) [
+      "${linuxXorg.libX11.out}/lib"
+    ])
+    (lib.optionals (linuxXorg ? libxcb) [
+      "${linuxXorg.libxcb.out}/lib"
+    ])
+    (lib.optionals (linuxXorg ? xcbutil) [
+      "${linuxXorg.xcbutil.out}/lib"
+    ])
+    (lib.optionals (linuxXorg ? xcbutilimage) [
+      "${linuxXorg.xcbutilimage.out}/lib"
+    ])
+    (lib.optionals (linuxXorg ? xcbutilrenderutil) [
+      "${linuxXorg.xcbutilrenderutil.out}/lib"
+    ])
+    (lib.optionals (linuxXorg ? xcbutilwm) [
+      "${linuxXorg.xcbutilwm.out}/lib"
+    ])
+    (lib.optionals (linuxXorg ? xcbutilkeysyms) [
+      "${linuxXorg.xcbutilkeysyms.out}/lib"
+    ])
+    (lib.optionals (linuxXorg ? libXcursor) [
+      "${linuxXorg.libXcursor.out}/lib"
+    ])
+    (lib.optionals (linuxXorg ? libXi) [
+      "${linuxXorg.libXi.out}/lib"
+    ])
+    (lib.optionals (linuxXorg ? libXrandr) [
+      "${linuxXorg.libXrandr.out}/lib"
+    ])
+  ];
   hostPkgConfigPath = builtins.getEnv "PKG_CONFIG_PATH";
   linuxPkgConfigSearchPaths =
-    linuxPkgConfigPaths
-    ++ lib.optional (hostPkgConfigPath != "") hostPkgConfigPath;
+    linuxPkgConfigPaths ++ lib.optional (hostPkgConfigPath != "") hostPkgConfigPath;
   hasLinuxPkgConfig = linuxPkgConfigSearchPaths != [ ];
-  linuxPkgConfigPath =
-    lib.concatStringsSep ":" linuxPkgConfigSearchPaths;
+  linuxPkgConfigPath = lib.concatStringsSep ":" linuxPkgConfigSearchPaths;
   hasLinuxLibraryPaths = linuxLibraryPaths != [ ];
-  linuxLdFlags =
-    lib.concatStringsSep " " (map (path: "-L${path}") linuxLibraryPaths);
+  linuxLdFlags = lib.concatStringsSep " " (map (path: "-L${path}") linuxLibraryPaths);
 in
 {
   devenv.root = lib.mkDefault (
@@ -118,28 +113,27 @@ in
     if envPwd != "" then envPwd else fallback
   );
 
-  env =
-    {
-      RUST_BACKTRACE = lib.mkDefault "1";
-      MACOSX_DEPLOYMENT_TARGET = lib.mkIf isDarwin "11.0";
-      PKG_CONFIG_ALLOW_CROSS = lib.mkDefault "1";
-    }
-    // lib.optionalAttrs (isDarwin && hasLinuxPkgConfig) {
-      PKG_CONFIG_PATH = linuxPkgConfigPath;
-      PKG_CONFIG_PATH_x86_64_unknown_linux_gnu = linuxPkgConfigPath;
-    }
-    // lib.optionalAttrs (isDarwin && hasLinuxLibraryPaths) {
-      NIX_LDFLAGS = linuxLdFlags;
-      NIX_LDFLAGS_x86_64_unknown_linux_gnu = linuxLdFlags;
-    }
-    // lib.optionalAttrs isDarwin {
-      CC_x86_64_unknown_linux_gnu = "${linuxCrossCc}/bin/${linuxTargetPrefix}cc";
-      CXX_x86_64_unknown_linux_gnu = "${linuxCrossCc}/bin/${linuxTargetPrefix}c++";
-      AR_x86_64_unknown_linux_gnu = "${linuxCrossBinutils}/bin/${linuxTargetPrefix}ar";
-      RANLIB_x86_64_unknown_linux_gnu = "${linuxCrossBinutils}/bin/${linuxTargetPrefix}ranlib";
-      CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER = "${linuxCrossCc}/bin/${linuxTargetPrefix}cc";
-      CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_AR = "${linuxCrossBinutils}/bin/${linuxTargetPrefix}ar";
-    };
+  env = {
+    RUST_BACKTRACE = lib.mkDefault "1";
+    MACOSX_DEPLOYMENT_TARGET = "11.0";
+    PKG_CONFIG_ALLOW_CROSS = lib.mkDefault "1";
+  }
+  // lib.optionalAttrs (isDarwin && hasLinuxPkgConfig) {
+    PKG_CONFIG_PATH = linuxPkgConfigPath;
+    PKG_CONFIG_PATH_x86_64_unknown_linux_gnu = linuxPkgConfigPath;
+  }
+  // lib.optionalAttrs (isDarwin && hasLinuxLibraryPaths) {
+    NIX_LDFLAGS = linuxLdFlags;
+    NIX_LDFLAGS_x86_64_unknown_linux_gnu = linuxLdFlags;
+  }
+  // lib.optionalAttrs isDarwin {
+    CC_x86_64_unknown_linux_gnu = "${linuxCrossCc}/bin/${linuxTargetPrefix}cc";
+    CXX_x86_64_unknown_linux_gnu = "${linuxCrossCc}/bin/${linuxTargetPrefix}c++";
+    AR_x86_64_unknown_linux_gnu = "${linuxCrossBinutils}/bin/${linuxTargetPrefix}ar";
+    RANLIB_x86_64_unknown_linux_gnu = "${linuxCrossBinutils}/bin/${linuxTargetPrefix}ranlib";
+    CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER = "${linuxCrossCc}/bin/${linuxTargetPrefix}cc";
+    CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_AR = "${linuxCrossBinutils}/bin/${linuxTargetPrefix}ar";
+  };
 
   languages.rust = {
     enable = true;
@@ -173,6 +167,12 @@ in
       xorg.libXcursor
       xorg.libXi
       xorg.libXrandr
+      xorg.libxcb
+      xorg.xcbutil
+      xorg.xcbutilimage
+      xorg.xcbutilrenderutil
+      xorg.xcbutilwm
+      xorg.xcbutilkeysyms
     ]
     ++ lib.optionals isDarwin [
       appleSdk
